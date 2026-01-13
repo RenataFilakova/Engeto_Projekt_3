a89530465eeaf2e9c546a36935955e7034db2707
Renata <r.filak10@gmail.com>
2026-01-13 19:10:17 +0100

    Add Kurzy navigation test and fix expectations

--- tests/test_engeto.py
+++ tests/test_engeto.py
@@ -1,18 +1,23 @@
-from playwright.sync_api import Page
+from playwright.sync_api import Page, expect
 
 
-def test_homepage_title(page: Page):
-    page.goto("https://engeto.cz")
-    assert "ENGETO" in page.title()
+def test_homepage_title(open_homepage: Page):
+    expect(open_homepage).to_have_title(
+        "Kurzy programování a dalších IT technologií | ENGETO"
+    )
 
 
-def test_courses_link_visible(page: Page):
-    page.goto("https://engeto.cz")
-    courses_link = page.get_by_role("link", name="Kurzy", exact=True)
-    assert courses_link.is_visible()
+def test_courses_link_visible(open_homepage: Page):
+    courses_link = open_homepage.get_by_role("link", name="Kurzy", exact=True)
+    expect(courses_link).to_be_visible()
 
 
-def test_footer_exists(page: Page):
-    page.goto("https://engeto.cz")
-    footer = page.locator("footer")
-    assert footer.is_visible()
+def test_courses_link_navigation(open_homepage: Page):
+    courses_link = open_homepage.get_by_role("link", name="Kurzy", exact=True)
+    courses_link.click()
+    expect(open_homepage).to_have_url("https://engeto.cz/prehled-kurzu/")
+
+
+def test_footer_exists(open_homepage: Page):
+    footer = open_homepage.locator("footer")
+    expect(footer).to_be_visible()
